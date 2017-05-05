1e2f61df7f29214144736862e85148c90dbdb202
Umang Shukla <Umang1089@gmail.com>
2017-05-05 10:02:32 -0500

    All correct except directors
    Need to finish directors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,12 +13,12 @@ class MoviesController < ApplicationController
   def create_row
     @movie = Movie.new
 
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
 
     @movie.save
 
@@ -30,6 +30,8 @@ class MoviesController < ApplicationController
   end
 
   def update_row
+    @movie = Movie.find(params[:id])
+
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
